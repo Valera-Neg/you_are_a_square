89fd2b6313d699e8e2969e13f36c47e009f2d638
Valera-Neg <nehovora.val@gmail.com>
2022-09-24 12:29:03 -0400

    first commit

---
+++ isSquare.js
@@ -0,0 +1,10 @@
+const isSquare = (n) => {
+  return Number.isInteger(Math.sqrt(n));
+}
+
+console.log(isSquare(-1)) //>  false
+console.log(isSquare(26)) //>  false
+console.log(isSquare(3)) //>  false
+console.log(isSquare(0)) //>  true
+console.log(isSquare(4)) //>  true
+console.log(isSquare(25)) //>  true
---
+++ isSquare_00.js
@@ -0,0 +1,10 @@
+const isSquare = (n) => {
+  return Math.sqrt(n) % 1 === 0;
+}
+
+console.log(isSquare(-1)) //>  false
+console.log(isSquare(26)) //>  false
+console.log(isSquare(3)) //>  false
+console.log(isSquare(0)) //>  true
+console.log(isSquare(4)) //>  true
+console.log(isSquare(25)) //>  true
